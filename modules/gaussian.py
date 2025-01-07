f634c63dec64a022069c5cf8a74b22fbd79ab217
AliValizade <alivalizade@gmail.com>
2025-01-07 10:01:42 +0330

    Review code

--- modules/gaussian.py
+++ modules/gaussian.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def gaussian_kernel(kernel_size, sigma):
-    # ایجاد کرنل گاوسی
+    # Create Gaussian Kernel
     ax = np.linspace(-(kernel_size // 2), kernel_size // 2, kernel_size)
     xx, yy = np.meshgrid(ax, ax)
     kernel = np.exp(-(xx**2 + yy**2) / (2 * sigma**2))
